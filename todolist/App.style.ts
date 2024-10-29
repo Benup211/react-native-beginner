5ed0c751c7aa403f2988d0c93816398b40cd1ca9
benup <benup211@gmail.com>
2024-10-29 22:56:06 +0545

    add:Layout for the todolist app and its style

---
+++ todolist/App.style.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const s = StyleSheet.create({
+    root: {
+        flex: 1,
+    },
+    app: {
+        flex: 1,
+        backgroundColor: "#F9F9F9",
+    },
+    header: { flex: 1 },
+    body: { flex: 5 },
+    footer: { height: 70 },
+});
